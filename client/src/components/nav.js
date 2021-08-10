89d8dda8c3923d643687d7a51216c19d536e5034
michaelshxw <michaelshxw@gmail.com>
2021-08-10 19:10:02 +0800

    delete test nav

---
+++ client/src/components/nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <nav>
+            <li>hey</li>
+            <li>hello</li>
+            <li>me</li>
+            <li>you</li>
+        </nav>
+    );
+}
