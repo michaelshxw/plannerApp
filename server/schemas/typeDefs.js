a7bbfe922acd501534f34bbf50d875b03728a1aa
michaelshxw <michaelshxw@gmail.com>
2021-08-10 20:03:19 +0800

    update typeDefs with template code

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,26 +3,36 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   type User {
     _id: ID
-    name: String
+    username: String
     email: String
-    # There is now a field to store the user's password
     password: String
+    classes: [Classes]!
   }
-  # Set up an Auth type to handle returning data from a profile creating or user login
-  type Auth {
-    token: ID!
-    user: User
-  }
-  type Query {
-    users: [User]!
-    user(userId: ID!): User
-  }
-  type Mutation {
-    # Set up mutations to handle creating a profile or logging into a profile and return Auth type
-    addUser(name: String!, email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth
-    removeProfile(profileId: ID!): Profile
+
+  type Class {
+    _id: ID
     
+  }
+
+  type Homework {
+
+  }
+
+  type Auth {
+
+  }
+
+  type Query {
+
+  }
+
+  type Mutation {
+    addUser: 
+    login:
+    addClass:
+    addHomework:
+    removeClass:
+    removeHomework: 
   }
 `;
 
