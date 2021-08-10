bfcfb55660cab67820bf8d617764d57ba8beb331
michaelshxw <michaelshxw@gmail.com>
2021-08-10 20:04:51 +0800

    update auth file with authMiddleware (start, not finished)

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,6 +4,10 @@ const secret = 'donttellanyonethissecretorelseyoullbecursedfortherestofyourlife'
 const expiration = '1h';
 
 module.exports = {
+  authMiddleware: function ({ req }) {
+    // code here
+  },
+  
   signToken: function ({ email, name, _id }) {
     const payload = { email, name, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
