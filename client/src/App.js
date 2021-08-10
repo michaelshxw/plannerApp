d446c75c736e78aaa03479481d38e1a146a56261
michaelshxw <michaelshxw@gmail.com>
2021-08-10 19:09:45 +0800

    app.js now complete minus correct components and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,51 @@
 import React from "react";
-import Login from './components/login'
-import Header from './components/header'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+
+//import components 
+import Header from './components/header';
+import Nav from './components/nav'
+
+//import pages
+
+
+//import apolloserver
 import {
-  ApolloClient, 
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink
 } from '@apollo/client';
+
 import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+    uri: '/graphql',
+});
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('id_token');
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    };
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
 });
 
 function App() {
-
-  return (
-    <ApolloProvider client={client}>
-      <Header />
-      <Login />
-    </ApolloProvider>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+                <Nav />
+                <Header />
+            </Router>
+        </ApolloProvider>
+    )
 }
 
 export default App;
