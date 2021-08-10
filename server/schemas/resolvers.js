6f34d33191466439b6ba05234c3c8242a7e0b601
michaelshxw <michaelshxw@gmail.com>
2021-08-10 20:01:07 +0800

    update resolver with template code

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,47 +1,57 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { Users: User } = require('../models');
+const { User, Classes, Homework } = require('../models');
 const { signToken } = require('../utils/auth');
 
+// resolvers object here
 const resolvers = {
   Query: {
     users: async () => {
-      return User.find();
+      return
+      // code here
     },
-
-    user: async (parent, { userId }) => {
-      return User.findOne({ _id: userId });
+    user: async () => {
+      return
+      // code here
     },
+    classes: async () => {
+      // code here
+    },
+    class: async () => {
+      // code here
+    },
+    allHomework: async () => {
+      // code here
+    },
+    homework: async () => {
+      // code here
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('????');
+      }
+      throw new AuthenticationError("Error: You need to be logged in.");
+    }
   },
-
   Mutation: {
-    addUser: async (parent, { name, email, password }) => {
-      const user = await User.create({ name, email, password });
-      const token = signToken(user);
-
-      return { token, user };
+    addUser: async () => {
+      // code here
     },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        throw new AuthenticationError('No user with this email address found!');
-      }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect password!');
-      }
-
-      const token = signToken(user);
-      return { token, user };
+    login: async () => {
+      // code here
     },
-
-      removeUser: async (parent, { userId }) => {
-      return User.findOneAndDelete({ _id: userId });
+    addClass: async () => {
+      // code here
+    }, 
+    addHomework: async () => {
+      // code here
     },
-    
-  },
-};
+    removeClass: async () => {
+      // code here
+    },
+    removeHomework: async () => {
+      // code here
+    },
+  }
+}
 
 module.exports = resolvers;
