28b73edbcef8b899b3dd9217185f1b3d7d9552b2
michaelshxw <michaelshxw@gmail.com>
2021-08-11 15:56:38 +0800

    typeDefs complete

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,28 +11,40 @@ const typeDefs = gql`
 
   type Class {
     _id: ID
-    
+    className: String
+    teacherName: String
+    time: Date
+    building: String
+    roomNumber: String
   }
 
   type Homework {
-
+    _id: ID
+    className: String
+    dueDate: Date
+    taskInfo: String
   }
 
   type Auth {
-
+    token: ID!
+    user: User
   }
 
   type Query {
-
+    users: [User]
+    user(username: String): User
+    classes(username: String): [Classes]
+    class(classId: ID!): Classes
+    me: user
   }
 
   type Mutation {
-    addUser: 
-    login:
-    addClass:
-    addHomework:
-    removeClass:
-    removeHomework: 
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addClass(className: String!, teacherName: String!, building: String!, roomNumber: String!): Classes
+    addHomework(homeworkId: ID!, className: String!, dueDate: Date!, taskInfo: String!): Homework
+    removeClass(classId: ID!): Classes
+    removeHomework(homeworkId: ID!): Homework
   }
 `;
 
