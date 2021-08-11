2d07e7e504c6a460e65c135d9e446737f24f6c2f
michaelshxw <michaelshxw@gmail.com>
2021-08-11 16:37:54 +0800

    add removeUser mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -54,6 +54,9 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
+    removeUser: async (parent, { userId }) => {
+      return User.findOneAndDelete({ _id: userId });
+    },
     addClass: async () => {
       // code here
     },
