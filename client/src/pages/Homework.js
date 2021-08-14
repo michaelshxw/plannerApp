b6913f74a25f2f37766ba5acbe1df6f93d6d1f04
michaelshxw <michaelshxw@gmail.com>
2021-08-15 02:15:22 +0800

    add homework page

---
+++ client/src/pages/Homework.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../components/Nav';
+import HomeworkComponent from '../components/HomeworkComponent'
+import AddClass from '../components/AddClass';
+
+export default function Homework() {
+    return (
+        <div>
+            <Nav />
+            <HomeworkComponent />
+        </div>
+    )
+}
