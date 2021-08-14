49a97010368909a3dfca9fb48d49ff2db10d251a
michaelshxw <michaelshxw@gmail.com>
2021-08-15 02:11:19 +0800

    add homework component

---
+++ client/src/components/HomeworkComponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AddClass from './AddClass';
+
+export default function HomeworkComponent() {
+    return (
+        <div className="appContainer bg-dark text-white">Homework Inserts Here</div>
+    );
+}
