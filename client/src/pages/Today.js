f0fa564a33d9e2d760d91330f80e16d07f835051
michaelshxw <michaelshxw@gmail.com>
2021-08-16 22:19:20 +0800

    heroku problems

--- client/src/pages/Today.js
+++ client/src/pages/Today.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Nav from '../../src/components/Nav';
+import Nav from '../components/Nav';
 import TodayComponent from '../components/TodayComponent';
 
 export default function Today() {
